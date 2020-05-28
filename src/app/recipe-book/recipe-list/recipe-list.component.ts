3f183977071ded2b2ee683c6f9f491466cc7457c
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-05-28 12:13:52 +0200

    Passing Data with Property Binding

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -12,6 +12,8 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
     new Recipe('A Test 2 Recipe', 'This is simply a second test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
+
+  @Output() recipe: Recipe;
   constructor() { }
 
   ngOnInit(): void {
