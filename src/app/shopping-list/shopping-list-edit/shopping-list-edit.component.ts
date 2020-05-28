1e673fe5169489c1a1b6d5d300cff61b691ca203
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-05-28 21:50:36 +0200

    User can add ingredients now

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputReference: ElementRef;
+  @ViewChild('amountInput') amountInputReference: ElementRef;
+  @Output() ingredientEmitter = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddClick(nameInput: string, amountInput: number) {
+    const ingName = this.nameInputReference.nativeElement.value;
+    const ingAmount = this.amountInputReference.nativeElement.value;
+    const ingredient = new Ingredient(ingName, ingAmount)
+    console.log(ingredient)
+    this.ingredientEmitter.emit(ingredient)
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,9 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientReceived(ingredient: Ingredient) {
+    this.ingredients.push(ingredient)
+    console.log(this.ingredients)
+  }
+
 }
