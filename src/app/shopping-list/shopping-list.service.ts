bd0f012f608ed0a845bd440e8a789dd09b711bfd
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-06-20 20:23:06 +0200

    Resetting form

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -28,4 +28,10 @@ export class ShoppingListService {
     getIngredient(index: number) {
         return this.ingredients[index];
     }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+
+    }
 }
