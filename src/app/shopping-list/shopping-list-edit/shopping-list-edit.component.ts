d0d723febf5a4b8c2c22aa0888cf87c4887c790d
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-06-20 22:01:40 +0200

    Reset, Clear, Delete Ingredients in Form

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -56,6 +56,16 @@ export class ShoppingListEditComponent implements OnInit, OnDestroy{
     form.reset();
   }
 
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex)
+    this.onClear();
+  }
+
+  onClear() {
+    this.shoppingListForm.reset();
+    this.editMode = false;
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
