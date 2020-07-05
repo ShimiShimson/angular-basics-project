81d37a9cbcc87a3884943725b83e4256fcd0dc9c
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-07-05 14:52:53 +0200

    Deleting Ingredients

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RecipeBookService } from './recipe-book.service';
 
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.component.html',
   styleUrls: ['./recipe-book.component.css'],
-  providers: [RecipeBookService]
 })
 export class RecipeBookComponent {
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,17 +1,18 @@
-import { Component, Output, OnInit } from '@angular/core';
-
+import { Component, Output, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeBookService } from '../recipe-book.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit{
-  @Output() recipe: Recipe;
+export class RecipeListComponent implements OnInit, OnDestroy{
+  // @Output() recipe: Recipe;
   @Output() recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(
     private recipeBookService: RecipeBookService,
@@ -19,7 +20,7 @@ export class RecipeListComponent implements OnInit{
     private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.recipeBookService.recipesChanged
+    this.subscription = this.recipeBookService.recipesChanged
       .subscribe(
         (recipes: Recipe[]) => {
           this.recipes = recipes;
@@ -30,5 +31,9 @@ export class RecipeListComponent implements OnInit{
 
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy () {
+    this.subscription.unsubscribe();
   }
 }
